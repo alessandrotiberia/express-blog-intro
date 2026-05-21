b6069b2f09377a7d17f3e5352ec5f21ab64d8abc
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-21 16:29:25 +0200

    creato post, aggiunte img in locale. fatto anche punto 3 con get che restituisce json con il post

--- index.js
+++ index.js
@@ -8,19 +8,57 @@ app.use(express.static('public'));
 
 app.get("/", (request, response) => {
     response.send("server del mio blog");
-    
+
+});
+// nel terminale srivi node --watch index.js per far runnare server nel browser
+
+// punto 2, creare array dove inserire lista di 5 post
+//titolo, contenuto, immagine e tags (tags è un array di stringhe)
+const post = [
+    {
+        titolo: "il padrino",
+        contenuto: "film",
+        img: "./public/il-padrino.jpeg",
+        tags: ["tag0", "tag1", "tag2"]
+    },
+    {
+        titolo: "lost",
+        contenuto: "serie tv",
+        img: "./public/lost.jpeg",
+        tags: ["tag0", "tag1", "tag2"]
+    },
+    {
+        titolo: "scarface",
+        contenuto: "film",
+        img: "./public/scarface.jpeg",
+        tags: ["tag0", "tag1", "tag2"]
+    },
+    {
+        titolo: "dragon ball",
+        contenuto: "anime",
+        img: "./public/dgb.jpeg",
+        tags: ["tag0", "tag1", "tag2"]
+    },
+    {
+        titolo: "one piece",
+        contenuto: "anime",
+        img: "./public/one-piece.png",
+        tags: ["tag0", "tag1", "tag2"]
+    }
+];
+
+app.get('/bacheca', (request, response) => {
+    response.json(post);
 });
 
 
 
-
-
-app.listen(3000,(error) => {
+app.listen(3000, (error) => {
     if (error) {
         console.error("errore nel caricamento della pagina 3000");
-        
+
     } else {
         console.log("pagina 3000 caricata con successo");
-        
+
     }
 });
