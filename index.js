fc00aa401ca342023d43d6d7a24b60fdf4ab42e8
Alessandro Tiberia <tiberia7030@gmail.com>
2026-05-21 14:57:08 +0200

    inserito listen con controllo caricamento pagina

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+import express from "express";
+
 console.log("creato indexjs");
+const app = express();
+
+
+
+
+app.listen(3000,(error) => {
+    if (error) {
+        console.error("errore nel caricamento della pagina 3000");
+        
+    } else {
+        console.log("pagina 3000 caricata con successo");
+        
+    }
+})
